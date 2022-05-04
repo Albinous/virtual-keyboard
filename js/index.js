ee86bedbc45b422868b6524cbb3b0c1be7cba2ff
Albina Kadyrzhanova <albina200235@gmail.com>
2022-05-05 02:34:42 +0600

    feat: add textarea and container

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
-let userName = 'Sergey';
-console.log(userName);
+const BODY = document.querySelector('body');
+const TEXTAREA_INPUT = document.createElement('textarea');
+const CONTAINER = document.createElement('div');
+CONTAINER.classList.add('container');
+
+TEXTAREA_INPUT.cols = 100;
+TEXTAREA_INPUT.rows = 10;
+BODY.append(CONTAINER);
+CONTAINER.append(TEXTAREA_INPUT);
