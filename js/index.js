35e96494cf781a5ae9946e39f46640624b2bd871
Albina <190317039@stu.sdu.edu.kz>
2022-05-09 08:01:07 +0600

    working events

--- js/index.js
+++ js/index.js
@@ -6,7 +6,9 @@ const keyboardKeys = document.createElement('div');
 let row;
 let item;
 
-function createRow() {
+let keysArr = [];
+
+const createRow = () => {
     row = document.createElement('div');
     row.classList.add('row');
 }
@@ -20,18 +22,21 @@ class KeyName {
     }
 }
 
-let keys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\\', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'Shift2', 'Ctrl', 'Win', 'Alt', '', 'Alt2', 'Fn', 'Ctrl2'];
+let keys = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '&#9650', 'Shift2', 'Ctrl', 'Win', 'Alt', '', 'Alt2', 'Fn', '&#5130', '&#9660', '&#5125', 'Ctrl2'];
 
 for (let i = 0; i < keys.length; i += 1) {
     item = document.createElement('div');
     let keyDetails = new KeyName(keys[i]);
-    item.innerHTML = keyDetails.key;
     item.classList.add(keyDetails.className);
+    item.innerHTML = keyDetails.key;
+    item.setAttribute('keyName', item.innerHTML);
+    item.setAttribute('lowerName', item.innerHTML.toLowerCase());
+    keysArr.push(item);
     row.append(item);
     if (keys[i] === 'Backspace') {
         keyboardKeys.append(row);
         createRow();
-    } else if (keys[i] === '\\') {
+    } else if (keys[i] === 'Del') {
         keyboardKeys.append(row);
         createRow();
     } else if (keys[i] === 'Enter') {
@@ -75,20 +80,70 @@ for (let i = 0; i < keys.length; i += 1) {
     if (keys[i] === '\\') {
         item.classList.add('slash-key');
     }
+    if (keys[i] === '') {
+        item.classList.add('space-key');
+    }
 
 }
 
 function longKey(key) {
     return item.classList.add(`${key}-key`)
 }
+
+const keyDownEvent = (e) => {
+    for (let i = 0; i < keysArr.length; i++) {
+        if (e.key == keysArr[i].getAttribute('keyName') || e.key == keysArr[i].getAttribute('lowerName')) {
+            keysArr[i].classList.add('active');
+            keysArr[i].classList.remove('remove');
+        }
+
+        console.log(e.code);
+
+        if (e.code == 'Space' && keysArr[i].classList.contains('space-key')) {
+            keysArr[i].classList.add('active');
+            keysArr[i].classList.remove('remove');
+        }
+
+        if (e.code == 'ShiftLeft' && keysArr[i].classList.contains('shift-right')) {
+            keysArr[i].classList.remove('active');
+        }
+
+        if (e.code == 'ShiftRight' && keysArr[i].classList.contains('shift-left')) {
+            keysArr[i].classList.remove('active');
+        }
+
+        if (e.code == 'CapsLock' && keysArr[i].classList.contains('capslock-key')) {
+            keysArr[i].classList.toggle('active');
+        }
+    }
+}
+
+const keyUpEvent = (e) => {
+    for (let i = 0; i < keysArr.length; i++) {
+        if (e.key == keysArr[i].getAttribute('keyName') || e.key == keysArr[i].getAttribute('lowerName')) {
+            keysArr[i].classList.remove('active');
+            keysArr[i].classList.add('remove');
+        }
+        
+        if (e.code == 'Space' && keysArr[i].classList.contains('space-key')) {
+            keysArr[i].classList.remove('active');
+            keysArr[i].classList.add('remove');
+        }
+    }
+}
+
+window.addEventListener('keydown', keyDownEvent);
+window.addEventListener('keyup', keyUpEvent);
+
+console.log(keysArr);
     
 
 
 CONTAINER.classList.add('container');
 TEXTAREA_INPUT.cols = 100;
 TEXTAREA_INPUT.rows = 10;
-KEYBOARD.classList.add('keyboard_wrapper');
-keyboardKeys.classList.add('keyboard_keys');
+KEYBOARD.classList.add('keyboard-wrapper');
+keyboardKeys.classList.add('keyboard-keys');
 
 BODY.append(CONTAINER);
 CONTAINER.append(TEXTAREA_INPUT);
